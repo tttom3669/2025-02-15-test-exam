92490c1302372f075eda0635336c89a15264c496
tttom3669 <tttom3669@gmail.com>
2025-02-15 14:21:45 +0800

    update： 渲染使用者

--- app.js
+++ app.js
@@ -1,29 +1,33 @@
 function App() {
+  const [userData, setUserData] = React.useState([]);
+  React.useEffect(() => {
+    render();
+  }, []);
+
+  const render = async () => {
+    const res = await fetch(`https://randomuser.me/api/?results=10`);
+    const resJson = await res.json();
+    console.log(resJson);
+    setUserData(resJson.results);
+  };
+
   return (
     <div className="container mx-auto p-4">
       <div className="row">
-        <div className="col-md-4">
-          <div className="bg-light p-3">
-            <img
-              src="https://randomuser.me/api/portraits/women/61.jpg"
-              alt="頭像"
-              className="img-fluid rounded-circle"
-            />
-            <h2 className="mb-0">Mona Heen</h2>
-            <p className="mb-0">mona.heen@example.com</p>
-          </div>
-        </div>
-        <div className="col-md-4">
-          <div className="bg-light p-3">
-            <img
-              src="https://randomuser.me/api/portraits/women/10.jpg"
-              alt="頭像"
-              className="img-fluid rounded-circle"
-            />
-            <h2 className="mb-0">Susan Craig</h2>
-            <p className="mb-0">susan.craig@example.com</p>
-          </div>
-        </div>
+        {userData &&
+          userData.map((user) => (
+            <div className="col-md-4" key={user.email}>
+              <div className="bg-light p-3">
+                <img
+                  src={user.picture.medium}
+                  alt="頭像"
+                  className="img-fluid rounded-circle"
+                />
+                <h2 className="mb-0">{user.name.first} {user.name.last}</h2>
+                <p className="mb-0">{user.email}</p>
+              </div>
+            </div>
+          ))}
       </div>
     </div>
   );
